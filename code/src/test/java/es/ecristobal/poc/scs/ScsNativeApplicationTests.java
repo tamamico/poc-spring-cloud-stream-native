513eb633146105a747ed5bee970089fd24bf06c0
Esteban Cristóbal <ecristobalr@gmail.com>
2024-02-03 18:16:20 +0100

    chore: Minor changes in dependencies and pipelines

--- code/src/test/java/es/ecristobal/poc/scs/ScsNativeApplicationTests.java
+++ code/src/test/java/es/ecristobal/poc/scs/ScsNativeApplicationTests.java
@@ -3,7 +3,6 @@ package es.ecristobal.poc.scs;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
@@ -14,23 +13,23 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.redpanda.RedpandaContainer;
 
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.testcontainers.utility.DockerImageName.parse;
 
 @Testcontainers
 @SpringBootTest
 class ScsNativeApplicationTests {
 
     @Container
-    private static RedpandaContainer container = new RedpandaContainer(
-            "docker.redpanda.com/redpandadata/redpanda:v23.3.4");
+    private static KafkaContainer container = new KafkaContainer(parse("confluentinc/cp-kafka:7.5.3")).withKraft();
 
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
