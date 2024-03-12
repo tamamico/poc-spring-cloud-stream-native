74532e1c1140e9f900cab33cf41ff31ca8207221
Esteban Cristóbal <ecristobalr@gmail.com>
2024-03-12 21:06:41 +0100

    fix: ScsNativeApplication made public

--- code/src/main/java/es/ecristobal/poc/scs/ScsNativeApplication.java
+++ code/src/main/java/es/ecristobal/poc/scs/ScsNativeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class ScsNativeApplication {
+public class ScsNativeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScsNativeApplication.class, args);
