37715ae37e6c60d0cf514465b4df3f6221b54a72
Esteban Cristóbal <ecristobalr@gmail.com>
2024-03-26 22:37:28 +0100

    test: Integration tests working with native image and schema registry

--- code/src/test/java/es/ecristobal/poc/scs/ScsNativeApplicationIT.java
+++ code/src/test/java/es/ecristobal/poc/scs/ScsNativeApplicationIT.java
@@ -8,22 +8,26 @@ import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.containers.wait.strategy.WaitStrategy;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.utility.DockerImageName;
 
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Map;
 
+import static java.lang.String.format;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
@@ -35,26 +39,47 @@ import static org.springframework.kafka.test.utils.KafkaTestUtils.consumerProps;
 import static org.springframework.kafka.test.utils.KafkaTestUtils.producerProps;
 import static org.testcontainers.utility.DockerImageName.parse;
 
-@Testcontainers
 class ScsNativeApplicationIT {
 
-    private static final String SCHEMA_REGISTRY_URL = "mock://";
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScsNativeApplicationIT.class);
 
-    @Container
-    private static KafkaContainer kafka =
-            new KafkaContainer(parse("confluentinc/cp-kafka:7.5.3")).withKraft();
+    private static final DockerImageName KAFKA_IMAGE           = parse("confluentinc/cp-kafka:latest");
+    private static final DockerImageName SCHEMA_REGISTRY_IMAGE = parse("confluentinc/cp-schema-registry:latest");
+    private static final DockerImageName APP_IMAGE             = parse("poc-scs-native:0.1.0-SNAPSHOT");
 
-    @Container
-    private static GenericContainer<?> app = new GenericContainer<>(parse("poc-scs-native:0.1.0-SNAPSHOT"))
-            .dependsOn(kafka);
+    private static final Network             network  = Network.newNetwork();
+    private static final KafkaContainer      kafka    = new KafkaContainer(KAFKA_IMAGE)
+            .withNetwork(network)
+            .withKraft();
+    private static final GenericContainer<?> registry = new GenericContainer<>(SCHEMA_REGISTRY_IMAGE)
+            .withNetwork(network)
+            .withExposedPorts(8081);
+    private static final GenericContainer<?> app      = new GenericContainer<>(APP_IMAGE)
+            .withNetwork(network);
+
+    private static String schemaRegistryUrl;
 
     @BeforeAll
     public static void setUp() {
         kafka.start();
-        app.withEnv("SPRING_CLOUD_STREAM_KAFKA_BINDER_BROKERS", kafka.getBootstrapServers())
-           .withEnv("SPRING_CLOUD_STREAM_KAFKA_BINDER_CONSUMERPROPERTIES_SCHEMA_REGISTRY_URL", SCHEMA_REGISTRY_URL)
-           .withEnv("SPRING_CLOUD_STREAM_KAFKA_BINDER_PRODUCERPROPERTIES_SCHEMA_REGISTRY_URL", SCHEMA_REGISTRY_URL)
+        registry.withEnv("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS", format("%s:9092", kafka.getContainerName()))
+                .withEnv("SCHEMA_REGISTRY_HOST_NAME", "schema_registry")
+                .start();
+        schemaRegistryUrl = format("http://%s:%d", registry.getHost(), registry.getFirstMappedPort());
+        final String dockerSchemaRegistryUrl = format("http:/%s:8081", registry.getContainerName());
+        app.withEnv("SPRING_CLOUD_STREAM_KAFKA_BINDER_BROKERS", kafka.getContainerName())
+           .withEnv("SPRING_CLOUD_STREAM_KAFKA_BINDER_CONSUMERPROPERTIES_SCHEMA_REGISTRY_URL", dockerSchemaRegistryUrl)
+           .withEnv("SPRING_CLOUD_STREAM_KAFKA_BINDER_PRODUCERPROPERTIES_SCHEMA_REGISTRY_URL", dockerSchemaRegistryUrl)
            .start();
+        final Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(LOGGER).withSeparateOutputStreams();
+        app.followOutput(logConsumer);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        app.stop();
+        registry.stop();
+        kafka.stop();
     }
 
     @Test
@@ -62,18 +87,18 @@ class ScsNativeApplicationIT {
         final Map<String, Object> producerProperties = producerProps(kafka.getBootstrapServers());
         producerProperties.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         producerProperties.put(VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        producerProperties.put("schema.registry.url", SCHEMA_REGISTRY_URL);
+        producerProperties.put("schema.registry.url", schemaRegistryUrl);
         final ProducerFactory<String, Input> producerFactory = new DefaultKafkaProducerFactory<>(producerProperties);
         final KafkaTemplate<String, Input>   template        = new KafkaTemplate<>(producerFactory, true);
         template.setDefaultTopic("input-test");
-        template.sendDefault(Input.newBuilder().setName("Steve").build());
         final Map<String, Object> consumerProperties = consumerProps(kafka.getBootstrapServers(), "test", "false");
         consumerProperties.put(AUTO_OFFSET_RESET_CONFIG, "earliest");
         consumerProperties.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         consumerProperties.put(VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
-        consumerProperties.put("schema.registry.url", SCHEMA_REGISTRY_URL);
+        consumerProperties.put("schema.registry.url", schemaRegistryUrl);
         consumerProperties.put("specific.avro.reader", true);
         final DefaultKafkaConsumerFactory<String, Output> cf = new DefaultKafkaConsumerFactory<>(consumerProperties);
+        template.sendDefault(Input.newBuilder().setName("Steve").build());
         try(Consumer<String, Output> consumer = cf.createConsumer()) {
             consumer.subscribe(Collections.singleton("output-test"));
             final ConsumerRecords<String, Output> records = consumer.poll(Duration.ofSeconds(10));
