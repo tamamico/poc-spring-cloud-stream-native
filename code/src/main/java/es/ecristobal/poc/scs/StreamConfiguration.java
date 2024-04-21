aeafd255ec62a60cbf1eee4a10e6fc65666ca75b
Esteban Cristóbal <ecristobalr@gmail.com>
2024-04-21 22:34:08 +0200

    chore: Updated logging and Build Cache extension

--- code/src/main/java/es/ecristobal/poc/scs/StreamConfiguration.java
+++ code/src/main/java/es/ecristobal/poc/scs/StreamConfiguration.java
@@ -2,8 +2,6 @@ package es.ecristobal.poc.scs;
 
 import es.ecristobal.poc.scs.avro.Input;
 import es.ecristobal.poc.scs.avro.Output;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
@@ -13,12 +11,10 @@ import java.util.function.Function;
 @Configuration
 class StreamConfiguration {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(StreamConfiguration.class);
-
     @Bean
     Function<Flux<Input>, Flux<Output>> sayHi() {
-        return flux -> flux.doOnNext(name -> LOGGER.info("Received name: {}", name))
-                           .map(message -> Output.newBuilder().setMessage("Hello, {}!".replace("{}", message.getName())).build());
+        return flux -> flux.log()
+                .map(message -> Output.newBuilder().setMessage("Hello, {}!".replace("{}", message.getName())).build());
     }
 
 }
