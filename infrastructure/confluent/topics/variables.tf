1ad36d1b9d6e061739b411067fedaf9315a911ec
Esteban Cristóbal <ecristobalr@gmail.com>
2024-05-24 23:12:32 +0200

    fix(infrastructure): Updated variables for modules

--- infrastructure/confluent/topics/variables.tf
+++ infrastructure/confluent/topics/variables.tf
@@ -1,3 +1,16 @@
-variable "environment" {
+variable "cluster_id" {
   type = string
+}
+
+variable "cluster_rest_endpoint" {
+  type = string
+}
+
+variable "api_key_id" {
+  type = string
+}
+
+variable "api_key_secret" {
+  type      = string
+  sensitive = true
 }
