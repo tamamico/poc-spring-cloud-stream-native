200286f50bb80f7f328bd1958bf3f8260386f207
tamamico <147539527+tamamico@users.noreply.github.com>
2024-05-30 15:42:49 +0200

    [skip ci] Update main.tf
    Signed-off-by: tamamico <147539527+tamamico@users.noreply.github.com>

--- infrastructure/confluent/main.tf
+++ infrastructure/confluent/main.tf
@@ -11,10 +11,39 @@ resource "confluent_environment" "development" {
   display_name = "development"
 }
 
+resource "confluent_service_account" "env-admin" {
+  display_name = "env-admin"
+  description  = "Environment admin service account created by Terraform"
+}
+
+resource "confluent_role_binding" "env-admin" {
+  principal   = "User:${confluent_service_account.env-admin.id}"
+  role_name   = "EnvironmentAdmin"
+  crn_pattern = confluent_environment.development.resource_name
+}
+
 module "cluster" {
   source = "./cluster"
 
   environment = confluent_environment.development.id
+}
+
+resource "confluent_api_key" "env-admin" {
+  display_name = "env-admin-api-key"
+  description  = "Environment manager API Key"
+  owner {
+    id          = confluent_service_account.env-admin.id
+    api_version = confluent_service_account.env-admin.api_version
+    kind        = confluent_service_account.env-admin.kind
+  }
+  managed_resource {
+    id          = module.cluster.cluster.id
+    api_version = module.cluster.cluster.api_version
+    kind        = module.cluster.cluster.kind
+    environment {
+      id = var.environment
+    }
+  }
 }
 
 module "topics" {
@@ -25,8 +54,8 @@ module "topics" {
     rest_endpoint = module.cluster.cluster.rest_endpoint
   }
   api_key = {
-    id     = module.cluster.api_key.id
-    secret = module.cluster.api_key.secret
+    id     = confluent_api_key.env_admin.id
+    secret = confluent_api_key.env_admin.secret
   }
 }
 
@@ -35,8 +64,8 @@ module "schema-registry" {
 
   environment = confluent_environment.development.id
   api_key = {
-    id     = module.cluster.api_key.id
-    secret = module.cluster.api_key.secret
+    id     = confluent_api_key.env_admin.id
+    secret = confluent_api_key.env_admin.secret
   }
 }
 
@@ -52,8 +81,8 @@ module "users" {
     rest_endpoint = module.cluster.cluster.rest_endpoint
   }
   api_key = {
-    id     = module.cluster.api_key.id
-    secret = module.cluster.api_key.secret
+    id     = confluent_api_key.env_admin.id
+    secret = confluent_api_key.env_admin.secret
   }
   input-topic     = module.topics.input-men-topic.topic_name
   output-topic    = module.topics.output-topic.topic_name
