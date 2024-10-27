c92f3e3b5630c2c357e437c9a8d71560d5f2ec53
Esteban Cristóbal <ecristobalr@gmail.com>
2024-10-27 22:30:58 +0100

    fix(infrastructure): Added  missing outputs

--- infrastructure/confluent/schema-registry/outputs.tf
+++ infrastructure/confluent/schema-registry/outputs.tf
@@ -1,3 +1,21 @@
 output "schema-registry-url" {
   value = data.confluent_schema_registry_cluster.schema-registry.rest_endpoint
 }
+
+output "poc-username" {
+  value = confluent_api_key.schema-registry-poc-user.id
+}
+
+output "poc-password" {
+  value = confluent_api_key.schema-registry-poc-user.secret
+  sensitive = true
+}
+
+output "test-username" {
+  value = confluent_api_key.schema-registry-poc-test.id
+}
+
+output "test-password" {
+  value = confluent_api_key.schema-registry-poc-test.secret
+  sensitive = true
+}
