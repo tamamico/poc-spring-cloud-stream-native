2853be83b63db150460b378d08c21aa348460dad
Esteban Cristóbal <ecristobalr@gmail.com>
2025-09-25 15:49:43 +0200

    chore(code): Version bump and Reactor Context first draft
    Signed-off-by: Esteban Cristóbal <ecristobalr@gmail.com>

--- code/src/main/java/es/ecristobal/poc/scs/StreamConfiguration.java
+++ code/src/main/java/es/ecristobal/poc/scs/StreamConfiguration.java
@@ -1,10 +1,13 @@
 package es.ecristobal.poc.scs;
 
+import java.util.List;
 import java.util.function.Function;
 
 import es.ecristobal.poc.scs.avro.Input;
 import es.ecristobal.poc.scs.avro.Output;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
@@ -13,13 +16,22 @@ import reactor.kafka.receiver.ReceiverOffset;
 
 import static java.util.Optional.ofNullable;
 
+import static io.micrometer.context.ContextRegistry.getInstance;
 import static org.springframework.kafka.support.KafkaHeaders.ACKNOWLEDGMENT;
 import static org.springframework.kafka.support.KafkaHeaders.KEY;
 import static org.springframework.messaging.support.MessageBuilder.withPayload;
+import static reactor.core.publisher.Hooks.enableAutomaticContextPropagation;
 
 @Slf4j
 @Configuration
 class StreamConfiguration {
+
+    private static final List<String> FIELDS = List.of("user.name");
+
+    @PostConstruct
+    public void init() {
+        enableAutomaticContextPropagation();
+    }
 
     @Bean
     Greeter greeter() {
@@ -30,7 +42,11 @@ class StreamConfiguration {
     Function<Flux<Message<Input>>, Flux<Message<Output>>> greet(
             final Greeter greeter
     ) {
-        return flux -> flux.doOnNext(input -> log.atInfo()
+        FIELDS.forEach(field -> getInstance().registerThreadLocalAccessor(field, () -> MDC.get(field),
+                                                                          username -> MDC.put(field, username),
+                                                                          () -> MDC.remove(field)));
+        return flux -> flux.contextWrite(context -> context.put("user.name", "Pirindolo"))
+                           .doOnNext(input -> log.atInfo()
                                                  .setMessage("Greeting {}")
                                                  .addArgument(input.getPayload()
                                                                    .getWho())
@@ -39,9 +55,9 @@ class StreamConfiguration {
                                ofNullable(input.getHeaders()
                                                .get(ACKNOWLEDGMENT, ReceiverOffset.class)).ifPresent(
                                        ReceiverOffset::acknowledge);
-                               return withPayload(greeter.greet(input.getPayload()))
-                                       .setHeader(KEY, input.getHeaders().get("kafka_receivedMessageKey"))
-                                       .build();
+                               return withPayload(greeter.greet(input.getPayload())).setHeader(KEY, input.getHeaders()
+                                                                                                         .get("kafka_receivedMessageKey"))
+                                                                                    .build();
                            });
     }
 
