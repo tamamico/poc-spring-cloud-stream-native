fc2a6f42a3057f77923cd733f76c8983a47d96ba
Esteban Cristóbal <ecristobalr@gmail.com>
2025-09-25 16:22:55 +0200

    feat(code): Added Slf4j MDC example
    Signed-off-by: Esteban Cristóbal <ecristobalr@gmail.com>

--- code/src/main/java/es/ecristobal/poc/scs/Greeter.java
+++ code/src/main/java/es/ecristobal/poc/scs/Greeter.java
@@ -2,14 +2,19 @@ package es.ecristobal.poc.scs;
 
 import es.ecristobal.poc.scs.avro.Input;
 import es.ecristobal.poc.scs.avro.Output;
+import lombok.extern.slf4j.Slf4j;
 
 import static java.lang.String.format;
 
+@Slf4j
 class Greeter {
 
     Output greet(
             final Input input
     ) {
+        log.atInfo()
+           .setMessage("Received person to greet")
+           .log();
         return Output.newBuilder()
                      .setMessage(format("Hello, %S!", input.getWho()))
                      .build();
--- code/src/main/java/es/ecristobal/poc/scs/StreamConfiguration.java
+++ code/src/main/java/es/ecristobal/poc/scs/StreamConfiguration.java
@@ -1,12 +1,10 @@
 package es.ecristobal.poc.scs;
 
-import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 import es.ecristobal.poc.scs.avro.Input;
 import es.ecristobal.poc.scs.avro.Output;
-import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,22 +14,14 @@ import reactor.kafka.receiver.ReceiverOffset;
 
 import static java.util.Optional.ofNullable;
 
-import static io.micrometer.context.ContextRegistry.getInstance;
 import static org.springframework.kafka.support.KafkaHeaders.ACKNOWLEDGMENT;
 import static org.springframework.kafka.support.KafkaHeaders.KEY;
 import static org.springframework.messaging.support.MessageBuilder.withPayload;
-import static reactor.core.publisher.Hooks.enableAutomaticContextPropagation;
 
-@Slf4j
 @Configuration
 class StreamConfiguration {
 
-    private static final List<String> FIELDS = List.of("user.name");
-
-    @PostConstruct
-    public void init() {
-        enableAutomaticContextPropagation();
-    }
+    private static final String KAFKA_KEY = "kafka_receivedMessageKey";
 
     @Bean
     Greeter greeter() {
@@ -42,23 +32,22 @@ class StreamConfiguration {
     Function<Flux<Message<Input>>, Flux<Message<Output>>> greet(
             final Greeter greeter
     ) {
-        FIELDS.forEach(field -> getInstance().registerThreadLocalAccessor(field, () -> MDC.get(field),
-                                                                          username -> MDC.put(field, username),
-                                                                          () -> MDC.remove(field)));
-        return flux -> flux.contextWrite(context -> context.put("user.name", "Pirindolo"))
-                           .doOnNext(input -> log.atInfo()
-                                                 .setMessage("Greeting {}")
-                                                 .addArgument(input.getPayload()
-                                                                   .getWho())
-                                                 .log())
-                           .map(input -> {
-                               ofNullable(input.getHeaders()
-                                               .get(ACKNOWLEDGMENT, ReceiverOffset.class)).ifPresent(
-                                       ReceiverOffset::acknowledge);
-                               return withPayload(greeter.greet(input.getPayload())).setHeader(KEY, input.getHeaders()
-                                                                                                         .get("kafka_receivedMessageKey"))
-                                                                                    .build();
-                           });
+        return flux -> flux.map(message -> {
+            final Input input = message.getPayload();
+            final Map<String, String> context = Map.of("user.name", input.getWho()
+                                                                         .toString());
+            try {
+                context.forEach(MDC::put);
+                ofNullable(message.getHeaders()
+                                  .get(ACKNOWLEDGMENT, ReceiverOffset.class)).ifPresent(ReceiverOffset::acknowledge);
+                return withPayload(greeter.greet(input)).setHeader(KEY, message.getHeaders()
+                                                                               .get(KAFKA_KEY))
+                                                        .build();
+            } finally {
+                context.keySet()
+                       .forEach(MDC::remove);
+            }
+        });
     }
 
 }
