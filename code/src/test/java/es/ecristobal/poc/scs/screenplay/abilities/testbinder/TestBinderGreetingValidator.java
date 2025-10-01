c4f69c2ed9af681479e3da0b95adb990d8f7cc15
Esteban Cristóbal <ecristobalr@gmail.com>
2025-10-01 15:52:48 +0200

    test(code): Fixed test to help in troubleshooting
    Signed-off-by: Esteban Cristóbal <ecristobalr@gmail.com>

--- code/src/test/java/es/ecristobal/poc/scs/screenplay/abilities/testbinder/TestBinderGreetingValidator.java
+++ code/src/test/java/es/ecristobal/poc/scs/screenplay/abilities/testbinder/TestBinderGreetingValidator.java
@@ -1,9 +1,17 @@
 package es.ecristobal.poc.scs.screenplay.abilities.testbinder;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import es.ecristobal.poc.scs.avro.Output;
 import es.ecristobal.poc.scs.screenplay.abilities.GreetingValidator;
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.binder.test.OutputDestination;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+@Slf4j
 public class TestBinderGreetingValidator
         implements GreetingValidator {
 
@@ -16,8 +24,16 @@ public class TestBinderGreetingValidator
 
     @Override
     public void with(final java.util.function.Consumer<String> assertions) {
-        assertions.accept(new String(this.outputDestination.receive()
-                                                           .getPayload()));
+        final byte[] response = this.outputDestination.receive()
+                                                      .getPayload();
+        log.info("Received response message: \"{}\"", new String(response));
+        try {
+            final Output output = Output.fromByteBuffer(ByteBuffer.wrap(response));
+            assertions.accept(output.getMessage()
+                                    .toString());
+        } catch(IOException e) {
+            fail("Error receiving response message", e);
+        }
 
     }
 
