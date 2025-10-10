68f7fe6f9e0907c40b76bbd6f1c46a9daf06f66a
Esteban Cristóbal <ecristobalr@gmail.com>
2025-10-10 22:58:17 +0200

    fix(code): Updated timestamp format from int to long
    Signed-off-by: Esteban Cristóbal <ecristobalr@gmail.com>

--- code/src/main/java/es/ecristobal/poc/scs/Greeter.java
+++ code/src/main/java/es/ecristobal/poc/scs/Greeter.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 import static java.lang.String.format;
-import static java.sql.Timestamp.valueOf;
 import static java.time.LocalDate.now;
 
 import static es.ecristobal.poc.scs.avro.Output.newBuilder;
@@ -18,9 +17,9 @@ class Greeter {
     Mono<Output> greet(
             final Input input
     ) {
-        return just(input.getWho()).doOnNext(name -> log.info("Received person name to greet"))
+        return just(input.getWho()).doOnNext(name -> log.info("Greeting {}", name))
                                    .map(name -> newBuilder().setMessage(format("Hello, %S!", name))
-                                                            .setDate(valueOf(now().atStartOfDay()).getNanos())
+                                                            .setDate(now().toEpochDay())
                                                             .build());
     }
 
