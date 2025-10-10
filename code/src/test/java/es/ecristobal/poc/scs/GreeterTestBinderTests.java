182f321abb5578f6afaca442435cb5efea03d87f
Esteban Cristóbal <ecristobalr@gmail.com>
2025-10-10 09:55:39 +0200

    test(code): Finally got it working!
    Signed-off-by: Esteban Cristóbal <ecristobalr@gmail.com>

--- code/src/test/java/es/ecristobal/poc/scs/GreeterTestBinderTests.java
+++ code/src/test/java/es/ecristobal/poc/scs/GreeterTestBinderTests.java
@@ -8,17 +8,9 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.cloud.stream.binder.test.EnableTestBinder;
 import org.springframework.cloud.stream.binder.test.InputDestination;
 import org.springframework.cloud.stream.binder.test.OutputDestination;
-import org.springframework.cloud.stream.schema.registry.avro.AvroSchemaMessageConverter;
-import org.springframework.cloud.stream.schema.registry.avro.AvroSchemaServiceManagerImpl;
-import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.converter.MessageConverter;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.util.MimeType;
 
 @Disabled
 @SpringBootTest
@@ -26,18 +18,6 @@ import org.springframework.util.MimeType;
 @SuppressWarnings("java:S2699")
 class GreeterTestBinderTests
         extends TestScenarios {
-
-    private static final MimeType AVRO_MIME_TYPE = MimeType.valueOf("avro/bytes");
-
-    @TestConfiguration
-    static class TestBinderConfiguration {
-
-        @Bean
-        MessageConverter avroMessageConverter() {
-            return new AvroSchemaMessageConverter(AVRO_MIME_TYPE, new AvroSchemaServiceManagerImpl());
-        }
-
-    }
 
     private final GreetingVisitor   greetingVisitor;
     private final GreetingValidator greetingValidator;
@@ -50,12 +30,6 @@ class GreeterTestBinderTests
         this.greetingValidator = TestBinderGreetingValidator.builder()
                                                             .outputDestination(output)
                                                             .build();
-    }
-
-    @DynamicPropertySource
-    static void setUp(final DynamicPropertyRegistry registry) {
-        registry.add("spring.cloud.stream.bindings.greet-out-0.producer.use-native-encoding", () -> false);
-        registry.add("spring.cloud.stream.bindings.greet-out-0.content-type", () -> AVRO_MIME_TYPE);
     }
 
     @Test
--- code/src/test/java/es/ecristobal/poc/scs/screenplay/abilities/testbinder/TestBinderGreetingValidator.java
+++ code/src/test/java/es/ecristobal/poc/scs/screenplay/abilities/testbinder/TestBinderGreetingValidator.java
@@ -1,19 +1,23 @@
 package es.ecristobal.poc.scs.screenplay.abilities.testbinder;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import es.ecristobal.poc.scs.avro.Output;
 import es.ecristobal.poc.scs.screenplay.abilities.GreetingValidator;
 import lombok.Builder;
-import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
 import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.messaging.Message;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
-@Slf4j
 public class TestBinderGreetingValidator
         implements GreetingValidator {
+
+    private static final Pattern RESPONSE = Pattern.compile("^.*payload=(.*), headers.*");
 
     private final OutputDestination outputDestination;
 
@@ -24,17 +28,26 @@ public class TestBinderGreetingValidator
 
     @Override
     public void with(final java.util.function.Consumer<String> assertions) {
-        final byte[] response = this.outputDestination.receive()
-                                                      .getPayload();
-        log.info("Received response message: \"{}\"", new String(response));
-        try {
-            final Output output = Output.fromByteBuffer(ByteBuffer.wrap(response));
-            assertions.accept(output.getMessage()
-                                    .toString());
-        } catch(IOException e) {
-            fail("Error receiving response message", e);
-        }
+        final Output output = this.getFrom(this.outputDestination.receive());
+        assertions.accept(output.getMessage()
+                                .toString());
+    }
 
+    private Output getFrom(final Message<byte[]> message) {
+        final Matcher matcher = RESPONSE.matcher(message.toString());
+        if(matcher.matches()) {
+            try {
+                final String              payload = matcher.group(1);
+                final DatumReader<Output> reader  = new SpecificDatumReader<>(Output.getClassSchema());
+                final Decoder decoder = DecoderFactory.get()
+                                                      .jsonDecoder(Output.getClassSchema(), payload);
+                return reader.read(null, decoder);
+            } catch(IOException e) {
+                throw new IllegalArgumentException(e);
+            }
+        } else {
+            throw new IllegalArgumentException("Response message does not match expected pattern");
+        }
     }
 
 }
